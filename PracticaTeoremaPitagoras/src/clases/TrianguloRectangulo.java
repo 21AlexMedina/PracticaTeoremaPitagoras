97d005325bbe1a4337b170e805932efb66cd08ed
AlexNoGay <ordnajelamedina21@gmail.com>
2025-05-20 16:25:01 +0200

    Añadidos los metodos y constructores

--- PracticaTeoremaPitagoras/src/clases/TrianguloRectangulo.java
+++ PracticaTeoremaPitagoras/src/clases/TrianguloRectangulo.java
@@ -1,27 +1,56 @@
 package clases;
 
 public class TrianguloRectangulo {
-double catetoA;
-double catetoB;
-double hipotenusa; 
+	double catetoA;
+	double catetoB;
+	double hipotenusa;
 
-
-
-public TrianguloRectangulo(int a, int b) throws IllegalArgumentException {
-	if (a <= 0 || b <= 0) {
-		throw new IllegalArgumentException("Los catetos no pueden ser negativos");
+	public TrianguloRectangulo(int catetoA, int catetoB) throws IllegalArgumentException {
+		if (catetoA <= 0 || catetoB <= 0) {
+			throw new IllegalArgumentException("Los catetos no pueden ser negativos");
+		}
+		this.catetoA = catetoA;
+		this.catetoB = catetoB;
+		hipotenusa = hipotenusa();
 	}
-	this.catetoA = catetoA;
-	this.catetoB = catetoB;
-	hipotenusa=hipotenusa();
-}
 
-public TrianguloRectangulo() {
-	this.catetoA=1;
-	this.catetoB=1;
-}
+	public TrianguloRectangulo() {
+		this.catetoA = 1;
+		this.catetoB = 1;
+	}
 
+	public double area() {
+		double area = (this.catetoA * this.catetoB) / 2.0;
+		return area;
+	}
 
+	public double hipotenusa() {
+		double hipotenusa = Math.sqrt(Math.pow(catetoA, 2) + Math.pow(catetoB, 2));
+		return hipotenusa;
+	}
 
+	public double perimetro() {
+		double perimetro = catetoA + catetoB + hipotenusa();
+		return perimetro;
+	}
 
+	public double getcatetoA() {
+		return this.catetoA;
+	}
+
+	public double getcatetoB() {
+		return this.catetoB;
+	}
+
+	public double getHipotenusa() {
+		return hipotenusa;
+	}
+
+	@Override
+	public String toString() {
+		String mensaje;
+		mensaje = "El cateto A es " + this.catetoA + " ,el cateto b es " + this.catetoB + " y la hipotenusa es "
+				+ hipotenusa;
+		return mensaje;
+	}
 }
