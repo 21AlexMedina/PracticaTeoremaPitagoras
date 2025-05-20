6f0de794456204fd2ab264e8d954c6827a302351
AlexNoGay <ordnajelamedina21@gmail.com>
2025-05-20 16:54:55 +0200

    Retoque ejercicio depuracion

--- PracticaTeoremaPitagoras/src/app/Principal.java
+++ PracticaTeoremaPitagoras/src/app/Principal.java
@@ -6,14 +6,13 @@ public class Principal {
 
 	public static void main(String[] args) {
 		TrianguloRectangulo t1 = new TrianguloRectangulo(3, 1);
-		TrianguloRectangulo t2 = new TrianguloRectangulo ();
+		TrianguloRectangulo t2 = new TrianguloRectangulo();
 
-		t1.area();
-		t1.perimetro();
-		t2.area();
-		t2.perimetro();
-		
-		
+		double t1area = t1.area();
+		double t1perimetro = t1.perimetro();
+		double t2area = t2.area();
+		double t2perimetro = t2.perimetro();
+
 	}
 
 }
